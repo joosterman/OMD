c58dce4264144117443702f97cc1d11e39016a7d
LearnDriver <bas.van.sambeek@gmail.com>
2012-08-20 00:00:36 +0200

    Different style for tweets

--- Open Monumentendag/war/scripts/social.js
+++ Open Monumentendag/war/scripts/social.js
@@ -2,7 +2,7 @@ function loadTweets(query){
 	var jsonObj = $.getJSON("http://search.twitter.com/search.json?callback=?", 
 			{
 				q: query,
-				rpp: 10
+				rpp: 40
 			}
 			,parseTweets
 	);
@@ -20,17 +20,23 @@ function parseTweets(tweets){
 		/*var text = tweets.results[i].text.replace('@(https?://([-\w\.]+)+(/([\w/_\.]*(\?\S+)?(#\S+)?)?)?)@', '<a href="$1">$1</a>') //turn URLs into links
 		.replace('/@(\w+)/', '<a href="http://twitter.com/$1">@$1</a>'); //turn twitter usernames into links*/
 		
-		result += '<li id="tweet-'+id+'" data-corners="false" data-shadow="false" data-wrapperels="div" data-icon="arrow-r" data-iconpos="right" data-theme="c" class="ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-li-has-count ui-li-has-thumb ui-btn-up-c">';
-    	result += '<div class="ui-btn-inner ui-li">';
-    	result += '<a href="https://mobile.twitter.com/'+tweets.results[i].from_user+'/status/'+tweets.results[i].id_str+'" class="ui-link-inherit" data-transition="slide">';
-    	result += '<img src="'+tweets.results[i].profile_image_url+'" class="ui-li-thumb">';
+		result += '<li id="tweet-'+id+'">';
+		result += '<a href="https://mobile.twitter.com/'+tweets.results[i].from_user+'/status/'+tweets.results[i].id_str+'" class="ui-link-inherit" data-transition="slide">';
+		//result += '<img src="'+tweets.results[i].profile_image_url+'">';
+		result += '<h3>@'+tweets.results[i].from_user+'</h3>';
+		result += '<p>'+text+'</p>';
+		result += '<p></p>';
+		result += '<p class="ui-li-aside"><strong>'+tweets.results[i].created_at+'</strong>PM</p>';
+		result += '</a>';
+		result += '</li>';
+		/*
     	result += '<h3 class="ui-li-heading">'+text+'</h3>';
     	result += '<p class="ui-li-desc">@'+tweets.results[i].from_user+'</p>';
     	result += '<p class="ui-li-desc">'+tweets.results[i].created_at+'</p>';
     	result += '</a>';
     	result += '<span class="ui-icon ui-icon-arrow-r ui-icon-shadow">&nbsp;</span>';
     	result += '</div">';
-    	result += '</li>';
+    	result += '</li>';*/
 
 		}
 	
--- Open Monumentendag/war/social.jsp
+++ Open Monumentendag/war/social.jsp
@@ -5,6 +5,7 @@
 		<a href="https://twitter.com/intent/tweet?text=%23omd12" data-icon="check" class="ui-btn-right">Tweet</a>
 	</div>	
 	<div id="tweets" data-role="content" class="ui-content">
-		<ul class="tweetsList ui-listview" data-role="listview"></ul>
+		<ul class="tweetsList ui-listview" data-role="listview">
+		</ul>
 	</div>
 </div>
